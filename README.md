900790dc75870e963839133a10afc352f73b0bda
Mariuszow <cichoszmariusz@gmail.com>
2016-06-13 14:33:58 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -2,34 +2,32 @@
 
 Protokó³ komunikacyjny obs³uguj¹cy kamerê.
 
-Wartoœci mog¹ byæ wpisywane zarówno w heksach jak i systemie dziesiêtnym. Przy wpisywaniu heksów nale¿y je poprzedziæ "0x".
+Wartoœci mog¹ byæ wpisywane za pomoc¹ komend lub w trybie odczytu szesnastkowo. Przy wpisywaniu heksów nale¿y je poprzedziæ "0x".
 
 Do dyspozycji mamy:
 4 kamery o adresach : 1(0x01) 2(0x02) 3(0x03) 4(0x04)
 
 Funkcje wystêpuj¹ce to:
-*POWER ON/OFF (kod funkcji to 0xA lub 10)- funkcja przyjmuje argumenty 0 dla off i 1 dla on
-*ZOOM LEVEL(1-5)(kod funkcji to 0xB lub 11)- - funkcja przyjmuje argumenty od 0 do 5 dla poszczególnych poziomów zbli¿enia
-*LIGHT ON/OFF/FLASH (kod funkcji to 0xC lub 12)-- funkcja przyjmuje argumenty 0 dla off 1 dla on i 2 b³ysku
+*POWER ON/OFF (kod funkcji to POW a adres funkcji to 0x0A )- funkcja przyjmuje argumenty 0 dla off i 1 dla on
+*LIGHT ON/OFF/FLASH (kod funkcji to LGH a adres funkcji to 0x0B )-- funkcja przyjmuje argumenty 0 dla off 1 dla on i 2 b³ysku
 
 Instrukcja obs³ugi: 
-1. W oknie podajesz komende dla jednej z trzech funkcji (dla POWER jest to POW, dla ZOOM jest to ZOO a dla LIGHT LGH).
-2. W wyniku wpisania komendy np. zad.exe POW otrzymujesz: komendê wpisan¹, informacjê o poprawnoœci (0x01 gdy poprawna, 0x11 gdy b³êdna),
-    iloœæ argumentów jak¹ przyjmuje funkcja np. 0x02, oraz sumê kontroln¹.
-3. W trybie odczytu mo¿esz sprawdziæ poprawnoœæ wpisanych wartoœci np. zad.exe -r 1 10 1 lub te¿ zad.exe -r 0x01 0x0A 0x01. W wyniku wpisania funkcji otrzymasz:
-    0x01 0x0A 0x01 0xF3 0x00
+1. W oknie podajesz komende dla jednej z czterech kamer: CAM1 - CAM4.
+2. Po spacji wpisujemy komendê jednej z funkcji: POW lub LGH. 
+3. Nastêpnie argument funkcji ON lub OFF lub te¿ dodarkowo FLASH dla funkcji LGH.
+4. W trybie odczytu wpisujemy wartoœci które wyrzuci nam konsola. Poprzedzamy to -r .
 	
-Budowa ramki:[komenda_funkcji]
-Przyk³ad: zad.exe LGH
-Czyli: Funkcja LIGHT
-Wynik:LGH 0x01 0x03 0xF3
-Czyli:[komenda_funkcji] [kod_b³êdu] [iloœæ_argumentów] [suma_kontrolna]
+Budowa ramki:[komenda_kamery] [komenda_funkcji] [argument_funkcji]
+Przyk³ad: zad.exe CAM1 POW ON
+Czyli: Kamera nr 1, funkcja POWER, argument ON
+Wynik:0x01 0x0A 0x01 0xF3
+Czyli:[adres_urz¹dzenia] [adres_funkcji] [argument_funkcji] [suma_kontrolna1]
 
-Budowa ramki:[-r] [adres_urz¹dzenia] [adres_funkcji] [argument_funkcji]
-Przyk³ad: zad.exe -r 1 10 1
-Czyli: tryb odczytu, kamera nr 1,  funkcja power, w³¹czona
-Wynik: 0x01 0x0A 0x01 0xF3 0x00
-Czyli:[adres_urz¹dzenia] [adres_funkcji] [argument_funkcji] [suma_kontrolna] [kod_b³êdu]
+Budowa ramki:[-r] [adres_urz¹dzenia] [adres_funkcji] [argument_funkcji] [suma_kontrolna1]
+Przyk³ad: zad.exe -r 0x01 0x0A 0x01 0xF3
+Czyli: tryb odczytu, kamera nr 1,  funkcja power, w³¹czona, suma kontrolna1
+Wynik: 0x01 0x0A 0x01 0xF3 0xE7 0x00
+Czyli:[adres_urz¹dzenia] [adres_funkcji] [argument_funkcji] [suma_kontrolna1] [suma_kontrolna2] [kod_b³êdu]
 
 KODY BLEDOW:
 0x00 EVERYTHING_OK
--- zad.cpp
+++ zad.cpp
@@ -5,73 +5,202 @@
 int check_device(long n);
 int check_fun(long f, long s);
 int check_sum(long a, long f, long ar);
-int check_errors(int a, int b, int c);
+int check_sum2(long checksum, long a, long f, long ar);
+int check_errors(int a, int b, int c, int d);
 
 int main(int argc, char* argv[])
 {
     if(argc<2)
       {
-          printf("Usage: zad.exe [r] [device_adress] [funktion_adress] [argument_of_funkction] or zad.exe [comand_of_function]");
+          printf("Usage:zad.exe [comand_of_device] [comand_of_function] [argument_of_function] and later -r and result first command ");
           return 0;
       }
-    else if (argc>5)
+    else if (argc>6)
     {
-          printf("too many argumenets Usage: zad.exe [r] [device_adress] [funktion_adress] [argument_of_funkction] or zad.exe [comand_of_function] ");
+          printf("too many argumenets Usage:zad.exe [comand_of_device] [comand_of_function] [argument_of_function] and later -r and result first command  ");
     }
     else
     {
     long device_adress;
     long function_adress;
     long arg_function;
-    const char* check_hex = "0x";
+    long check_sum1;
     const char* check_option = "-r";
-    const char* power = "POW";
-    const char* zoom = "ZOO";
-    const char* light = "LGH";
+
     if (strncmp(argv[1], check_option, 2) == 0)
     {
-        if (strncmp(argv[2], check_hex, 2) == 0)
         device_adress = strtol(argv[2],NULL ,16);
-        else
-        device_adress = strtol(argv[2],NULL ,10);
-        if (strncmp(argv[3], check_hex, 2) == 0)
         function_adress = strtol(argv[3],NULL ,16);
-        else
-        function_adress = strtol(argv[3],NULL ,10);
-        if (strncmp(argv[4], check_hex, 2) == 0)
         arg_function = strtol(argv[4],NULL ,16);
-        else
-        arg_function = strtol(argv[4],NULL ,10);
-
-        check_errors(check_sum(device_adress, function_adress, arg_function), check_fun(function_adress, arg_function),check_device(device_adress));
+        check_sum1 = strtol(argv[5],NULL ,16);
+        check_errors(check_sum2(check_sum1, device_adress, function_adress, arg_function), check_sum(device_adress, function_adress, arg_function), check_fun(function_adress, arg_function),check_device(device_adress));
+        return 0;
     }
     else
     {
-        if (strncmp(argv[1], power, 3) == 0)
+        if (strncmp(argv[1], "CAM1", 4) == 0)
         {
-            function_adress = 10;
-            printf("POW 0x00 0x02 ");
-            check_sum(0, function_adress, 0);
+            device_adress = 1;
+            if(strncmp(argv[2], "POW", 3) == 0)
+            {
+                function_adress = 10;
+                if(strncmp(argv[3], "ON", 2) == 0)
+                {
+                   arg_function = 1;
+                }
+                else if (strncmp(argv[3], "OFF", 3) == 0)
+                  {
+                    arg_function = 0;
+                  }
+                else
+                    printf("WRONG_COMMAND_OF_ARGUMENT ");
+            }
+            else if(strncmp(argv[2], "LGH", 3) == 0)
+            {
+               function_adress = 11;
+                 if(strncmp(argv[3], "ON", 2) == 0)
+                {
+                    arg_function = 1;
+                }
+                else if (strncmp(argv[3], "OFF", 3) == 0)
+                {
+                   arg_function = 0;
+                }
+                else if (strncmp(argv[3], "FLASH", 5) == 0)
+                {
+                    arg_function = 2;
+                }
+                else
+                    printf("[WRONG_COMMAND_OF_ARGUMENT]");
+            }
+            else
+            printf("WRONG_COMMAND_OF_FUNCTION");
         }
-        else if(strncmp(argv[1], zoom, 3) == 0)
+        else if (strncmp(argv[1], "CAM2", 4) == 0)
         {
-            function_adress = 11;
-            printf("ZOO 0x00 0x06 ");
-            check_sum(0, function_adress, 0);
+            device_adress = 2;
+            if(strncmp(argv[2], "POW", 3) == 0)
+            {
+                function_adress = 10;
+                if(strncmp(argv[3], "ON", 2) == 0)
+                {
+                    arg_function =1;
+                }
+                else if (strncmp(argv[3], "OFF", 3) == 0)
+                {
+                    arg_function =0;
+                }
+                else
+                    printf("WRONG_COMMAND_OF_ARGUMENT ");
+            }
+            else if(strncmp(argv[2], "LGH", 3) == 0)
+            {
+               function_adress =11;
+                 if(strncmp(argv[3], "ON", 2) == 0)
+                {
+                    arg_function = 1;
+                }
+                else if (strncmp(argv[3], "OFF", 3) == 0)
+                {
+                    arg_function = 0;
+                }
+                else if (strncmp(argv[3], "FLASH", 5) == 0)
+                {
+                    arg_function = 2;
+                }
+                else
+                    printf("[WRONG_COMMAND_OF_ARGUMENT]");
+            }
+            else
+            printf("WRONG_COMMAND_OF_FUNCTION");
         }
-        else if(strncmp(argv[1], light, 3) == 0)
+        else if (strncmp(argv[1], "CAM3", 4) == 0)
         {
-            function_adress = 12;
-            printf("LGH 0x00 0x03 ");
-            check_sum(0, function_adress, 0);
+            device_adress =3;
+            if(strncmp(argv[2], "POW", 3) == 0)
+            {
+                function_adress = 10;
+                if(strncmp(argv[3], "ON", 2) == 0)
+                {
+                    arg_function =1;
+                }
+                else if (strncmp(argv[3], "OFF", 3) == 0)
+                {
+                    arg_function =0;
+                }
+                else
+                    printf("WRONG_COMMAND_OF_ARGUMENT ");
+            }
+            else if(strncmp(argv[2], "LGH", 3) == 0)
+            {
+               function_adress =11;
+                 if(strncmp(argv[3], "ON", 2) == 0)
+                {
+                    arg_function = 1;
+                }
+                else if (strncmp(argv[3], "OFF", 3) == 0)
+                {
+                    arg_function = 0;
+                }
+                else if (strncmp(argv[3], "FLASH", 5) == 0)
+                {
+                    arg_function = 2;
+                }
+                else
+                    printf("[WRONG_COMMAND_OF_ARGUMENT]");
+            }
+            else
+            printf("WRONG_COMMAND_OF_FUNCTION");
+        }
+        else if (strncmp(argv[1], "CAM4", 4) == 0)
+        {
+            device_adress = 4;
+           if(strncmp(argv[2], "POW", 3) == 0)
+            {
+                function_adress = 10;
+                if(strncmp(argv[3], "ON", 2) == 0)
+                {
+                    arg_function =1;
+                }
+                else if (strncmp(argv[3], "OFF", 3) == 0)
+                {
+                    arg_function =0;
+                }
+                else
+                    printf("WRONG_COMMAND_OF_ARGUMENT ");
+            }
+            else if(strncmp(argv[2], "LGH", 3) == 0)
+            {
+               function_adress =11;
+                 if(strncmp(argv[3], "ON", 2) == 0)
+                {
+                    arg_function = 1;
+                }
+                else if (strncmp(argv[3], "OFF", 3) == 0)
+                {
+                    arg_function = 0;
+                }
+                else if (strncmp(argv[3], "FLASH", 5) == 0)
+                {
+                    arg_function = 2;
+                }
+                else
+                    printf("[WRONG_COMMAND_OF_ARGUMENT]");
+            }
+            else
+            printf("WRONG_COMMAND_OF_FUNCTION");
         }
         else
-        {
-            printf(argv[1]);
-            printf(" 0x05");
-        }
-    }
+            {
+                printf("WRONG COMMAND OF CAMERA");
+            }
+
+
+    check_device(device_adress);
+    check_fun(function_adress, arg_function);
+    check_sum(device_adress, function_adress, arg_function);
     return 0;
+    }
     }
 }
 int check_device(long n)
@@ -104,53 +233,57 @@ int check_device(long n)
 }
 int check_fun(long f, long s)
 {
-            if(f == 10)
-            {
-             printf("0x0A ");
-            if(s==0){
-                printf("0x00 ");
-                return 0;}
-            else if (s==1){
-                printf("0x01 ");
-                return 0;}
-            else
-                printf("[] ");
-                return 3;
-            }
-             else if(f == 11)
-             {
-                 printf("0xB ");
-                 if(s<5)
-             {printf("0x%.2X", s);
-             return 0;}
-             else
-             printf("[] ");
-                return 3;
-             }
-              else if(f == 12)
-              {
-                  printf("0x0C");
-                 if(s==0)
-                {
-                    printf("0x00 ");
-                   return 0;}
-                  else if (s==1)
-                    {
-                    printf("0x01 ");
-                    return 0;}
-                   else if (s==2){
-                    printf("0x02 ");
-                    return 0;}
-                   else
-                   printf("[] ");
-                   return 3;
-              }
-            else
-            {
-            printf("[] [] ");
-            return 2;
-            }
+    if(f == 10)
+    {
+      printf("0x0A ");
+        if(s==0)
+        {
+            printf("0x00 ");
+            return 0;
         }
+        else if (s==1)
+        {
+            printf("0x01 ");
+            return 0;
+        }
+        else
+        {
+            printf("[] ");
+            return 3;
+        }
+    }
+
+    else if(f == 11)
+    {
+            printf("0x0B ");
+
+        if(s==0)
+        {
+            printf("0x00 ");
+            return 0;
+        }
+        else if (s==1)
+        {
+            printf("0x01 ");
+            return 0;
+        }
+        else if (s==2)
+        {
+            printf("0x02 ");
+            return 0;
+        }
+        else
+        {
+            printf("[] ");
+            return 3;
+        }
+    }
+    else
+    {
+      printf("[] [] ");
+      return 2;
+    }
+}
 int check_sum(long a, long f, long ar)
 {
     int sum_end = 0xFF - a - f - ar;
@@ -161,7 +294,7 @@ int check_sum(long a, long f, long ar)
     else
     return 0;
 }
-int check_errors(int a, int b, int c)
+int check_errors(int a, int b, int c ,int d)
 {
     if (a>b)
         printf("0x0%d", a);
@@ -169,7 +302,21 @@ int check_errors(int a, int b, int c)
         printf("0x0%d", b);
     else if(c>a)
         printf("0x0%d", c);
+    else if(d>a)
+        printf("0x0%d", c);
     else
         printf("0x00");
         return 0;
+}
+int check_sum2(long checksum, long a, long f, long ar)
+{
+    int sum_end = 0x00 + checksum - a -f - ar ;
+
+    if(sum_end<0)
+    {printf("[] ");
+    return 4;}
+    else
+    {printf("0x%.2X ", sum_end);
+    return 0;}
+
 }
